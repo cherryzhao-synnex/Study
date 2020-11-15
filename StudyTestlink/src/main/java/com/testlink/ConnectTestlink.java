9afac1f6098c3e3e1d72b2a541333f1f5583df42
zhaohuan <316937488@qq.com>
2020-11-15 23:36:49 +0800

    add study testlink API

---
+++ StudyTestlink/src/main/java/com/testlink/ConnectTestlink.java
@@ -0,0 +1,12 @@
+package com.testlink;
+
+/**
+ * @Auther : zhaohuan
+ * @Date: 2020/11/15
+ * @Description: com.testlink
+ * @version: 1.0
+ */
+public class ConnectTestlink {
+    String testlinkUrl = "";
+
+}
